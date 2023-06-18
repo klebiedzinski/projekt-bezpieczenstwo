35783e13414886a231106949546f25f947a820a4
piotrwasilewski420 <piotr.wasilewski420@gmail.com>
2023-06-18 19:47:30 +0200

    siema

--- frontend/src/Keycloak.js
+++ frontend/src/Keycloak.js
@@ -1,10 +1,10 @@
 import Keycloak from "keycloak-js";
-const keycloak = new Keycloak({
- url: "http://localhost:8080/auth",
- realm: "keycloak-react-auth",
- clientId: "react",
- "ssl-required": "external",
- "public-client": true
-});
+const keycloak = new Keycloak(
+  {
+  url: 'http://localhost:8080/auth',
+  realm: 'keycloak-react-auth',
+  clientId: 'react',
+  }
+);
 
 export default keycloak;
